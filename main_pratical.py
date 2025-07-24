1e7404a6074eef92bcd9815cdc4cb1a05b0a882c
prajwal66 <prajwalsuryawanshi12@gmail.com>
2025-07-24 11:55:57 +0530

    Add new_practical

---
+++ main_pratical.py
@@ -0,0 +1,5 @@
+import pratical1 as Test
+#x = Test.add()
+#print(x)
+
+print(Test.add())
---
+++ pratical1.py
@@ -0,0 +1,6 @@
+import sys
+def add():
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    a = num1 + num2 
+    return a 
