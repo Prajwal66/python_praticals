92b8a9b951e4930a0d89720aa4735fdc226e8150
prajwal66 <prajwalsuryawanshi12@gmail.com>
2025-07-24 11:59:36 +0530

    pratices

--- main_pratical.py
+++ main_pratical.py
@@ -1,5 +1,2 @@
 import pratical1 as Test
-#x = Test.add()
-#print(x)
-
 print(Test.add())
--- pratical1.py
+++ pratical1.py
@@ -1,6 +1,8 @@
 import sys
+
+
 def add():
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
-    a = num1 + num2 
-    return a 
+    a = num1 + num2
+    return a
